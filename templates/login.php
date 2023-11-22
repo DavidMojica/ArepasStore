af2d79792302734f66144df759ab5c836350d367
David Mojica <davidmojicav@gmail.com>
2023-11-22 13:54:02 -0500

    22/11/23 IX Login layout I

--- templates/login.php
+++ templates/login.php
@@ -3,10 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Custom css -->
+    <link rel="stylesheet" href="self-css/general.css">
+    <link rel="stylesheet" href="self-css/login.css">
+    <link rel="stylesheet" href="../static/css/card-traveler.css">
+
+    
+    <title>Sabor Caro | Login</title>
+
+
 </head>
 <body>
+    <div class="sticky-header">
+        <div class="line-1"></div>
+        <div class="line-2"></div>
+        <div class="line-3"></div>
+    </div>
+    <div class="griddly">
+        <div class="card-traveler">
+        <div class="first-content">
+            <span>First</span>
+        </div>
+        <div class="second-content">
+        <span>Second</span>
+        </div>
+    </div>
     
     
+
+
+</div>
+
 </body>
 </html>
