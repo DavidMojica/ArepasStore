2b7e854c347f808033b7e6441bee761592a3b5f6
David Mojica <davidmojicav@gmail.com>
2023-11-22 14:21:37 -0500

    22/11/23 X Login layout II

--- templates/login.php
+++ templates/login.php
@@ -7,13 +7,14 @@
     <link rel="stylesheet" href="self-css/general.css">
     <link rel="stylesheet" href="self-css/login.css">
     <link rel="stylesheet" href="../static/css/card-traveler.css">
+    <link rel="stylesheet" href="../static/css/bg-dotted.css">
 
     
     <title>Sabor Caro | Login</title>
 
 
 </head>
-<body>
+<body class="bg-dotted">
     <div class="sticky-header">
         <div class="line-1"></div>
         <div class="line-2"></div>
@@ -22,10 +23,24 @@
     <div class="griddly">
         <div class="card-traveler">
         <div class="first-content">
-            <span>First</span>
+            <h2>Iniciar Sesion</h2>
+            <form action="">
+                <img src="../extras/logos/logo.png" alt="" class="logo"> <br>
+                <input type="text" name="i_username" id="i_username" placeholder="Nombre de usuario"> <br>
+                <input type="password" name="i_password" id="i_password" placeholder="Contraseña"> <br>
+                <button type="submit" name="i">Ingresar</button> 
+            </form>
+            <p>¿No tienes cuenta? <b>Registrate aquí!</b></p>
         </div>
         <div class="second-content">
-        <span>Second</span>
+            <h2>Registrarse</h2>
+            <form action="">
+                <img src="../extras/logos/logo.png" alt="" class="logo"> <br>
+                <input type="text" name="r_username" id="r_username" placeholder="Nombre de usuario">
+                <input type="password" name="r_password1" id="r_password1" placeholder="Contraseña">
+                <input type="password" name="r_password2" id="r_password2" placeholder="Conformar contraseña">
+                <button type="submit" name="r">Ingresar</button> 
+            </form>
         </div>
     </div>
     
