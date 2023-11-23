784b538ee58cd7de08b3c6cd51f5476af7d5909c
David Mojica <davidmojicav@gmail.com>
2023-11-23 09:34:24 -0500

    23/11/23 III log layout

--- templates/login.php
+++ templates/login.php
@@ -10,42 +10,61 @@
     <link rel="stylesheet" href="self-css/login.css">
     <link rel="stylesheet" href="../static/css/card-traveler.css">
     <link rel="stylesheet" href="../static/css/bg-dotted.css">
+    <link rel="stylesheet" href="../static/css/in-focus.css">
+    <link rel="stylesheet" href="../static/css/btn_type_A.css">
+    <link rel="stylesheet" href="../static/css/form_slashing.cs">
 
     <script src="../static/js/login.js" defer></script>    
     <title>Sabor Caro | Login</title>
 </head>
-<body class="bg-dotted">
-    <div class="sticky-header">
-        <div class="line-1"></div>
-        <div class="line-2"></div>
-        <div class="line-3"></div>
-    </div>
-    <div class="griddly">
-        <div class="card-traveler" id="l_card">
-            <div class="first-content" id="first-content">
-                <h2 class="col-white">Iniciar Sesion</h2>
-                <form action="" id="login_form">
-                    <img src="../extras/logos/logo.png" alt="" class="logo"> 
-                    <input type="text" name="i_username" id="i_username" placeholder="Nombre de usuario"> 
-                    <input type="password" name="i_password" id="i_password" placeholder="Contraseña"> 
-                    <p id="msg" class="small"></p>
-                    <button type="submit" name="i">Ingresar</button> 
-                </form>
-                <p class="small"><b id="b_change" class="col-white">Registrate aquí!</b></p>
-            </div>
-            <div class="second-content" id="second-content">
-                <h2>Registrarse</h2>
-                <form action="" id="register_form">
-                    <img src="../extras/logos/logo.png" alt="" class="logo"> <br>
-                    <p>¿Ya tienes cuenta? <b id="a_change">ingresa aquí</b></sp>
-                    <input type="text" name="r_username" id="r_username" placeholder="Nombre de usuario">
-                    <input type="password" name="r_password1" id="r_password1" placeholder="Contraseña">
-                    <input type="password" name="r_password2" id="r_password2" placeholder="Conformar contraseña">
-                    <p id="msg_r"></p>
-                    <button type="submit" name="r">Ingresar</button> 
-                </form> 
+<body class="bg_dotted">
+<nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="../index.php">
+                <img src="https://www.mintransporte.gov.co/info/mintransporte/media/pubInt/thumbs/thpub_700x400_10745.jpg" class="logo" alt="Gobernacion Antioquia">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li>
+                        <a href="../index.php">
+                            <button class="btn_type_A">
+                                <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                                Regresar
+                            </button>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
+    </nav>
+
+    
+    <main>
+        <div class="form-container">
+            <form action="" class="form" method="POST" id="loginForm">
+                <a class="navbar-brand logo_form" href="../index.php">
+                <img src="https://www.mintransporte.gov.co/info/mintransporte/media/pubInt/thumbs/thpub_700x400_10745.jpg" class="ans_logo" alt="Gobernacion Antioquia">
+                </a>
+            
+            
+                <h2>Iniciar Sesión</h2>
+                <div class="form-group">
+                    <label for="user">Usuario</label>
+                    <input type="text" name="user" id="user">
+                </div>
+                <div class="form-group">
+                    <label for="pass">Contraseña</label>
+                    <input type="password" name="pass" id="pass">
+                </div>
+                <p id="msg"></p>
+                <button class="form-submit-btn">Ingresar</button>
+            </form>
+        </div>
+    </main>
+    
 </body>
+</html>
 </html>
