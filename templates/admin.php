e224a34c0a49db022289bdc96022af723d0e6535
David Mojica <davidmojicav@gmail.com>
2023-11-27 12:47:57 -0500

    27/11/23 X Adm II

--- templates/admin.php
+++ templates/admin.php
@@ -1,9 +1,34 @@
+<?php
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['username']) || !$_SESSION['username']) {
+    header('Location: login.php');
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- JQUERY -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- favicon -->
+    <link rel="shortcut icon" href="../extras/logos/arepa.png" type="image/x-icon">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="self-css/general.css">
+    <link rel="stylesheet" href="self-css/pedidos.css">
+    <link rel="stylesheet" href="../static/css/bg-dotted.css">
+    <!-- Custom JS -->
+    <script src="../static/js/clases.js" defer></script>
+    
+    
+    <title>Sabor Caro | Admin</title>
 </head>
 <body>
     
--- templates/cart.php
+++ templates/cart.php
@@ -10,7 +10,6 @@ if (!isset($_SESSION['username']) || !$_SESSION['username']) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
